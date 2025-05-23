fbc0393d1c3f6499b20b7136ea436bbee5948ae6
nishikanta7819 <nishikantabehera7819.com>
2025-05-23 10:59:15 +0530

    updates

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -43,6 +43,24 @@ const Collection = () => {
       originalPrice: 1499,
       discountText: "53% off",
     },
+    {
+      id: 5,
+      image:
+        "https://veirdo.in/cdn/shop/files/1_13_8756eefd-b3b7-490b-b214-695f131da9c5.jpg?v=1734944479&width=533",
+      title: "Veirdo Original Beige Oversized",
+      discountPrice: 699,
+      originalPrice: 1499,
+      discountText: "53% off",
+    },
+    {
+      id: 6,
+      image:
+        "https://veirdo.in/cdn/shop/files/1_13_8756eefd-b3b7-490b-b214-695f131da9c5.jpg?v=1734944479&width=533",
+      title: "Veirdo Original Beige Oversized",
+      discountPrice: 699,
+      originalPrice: 1499,
+      discountText: "53% off",
+    },
     // Add more as needed
   ];
 
